2a5b689b1e49ef0b1e1751d323ff9b41244b06d1
ChicoTasso <tassomaisdogi@gmail.com>
2023-12-17 09:08:08 -0300

    pasta media

--- locadora/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Car, User, Reservation
-
-# Register your models here.
-
-admin.site.register(Car)
-admin.site.register(Reservation)
-admin.site.register(User)
